590a939522d9e83d6fc074f0ae3366750187260c
marc1107 <m.bohner99@gmail.com>
2022-04-17 19:04:28 +0200

    Aufgabe 2.3

--- Blatt02/2.3.py
+++ Blatt02/2.3.py
@@ -1,13 +1,11 @@
 allCombos = 10 * 10 * 10 * 10
 noZeroBegin = allCombos - (1 * 10 * 10 * 10)
+a = (1/10) * (1/10) * (1/10) * (1/10)
+b = (1/9) * (1/10) * (1/10) * (1/10)
 
 print("2.3.1:")
 print("a)", allCombos)
 print("b)", noZeroBegin)
 print("\n2.3.2")
-print("a)",
-      (1 / allCombos) + ((allCombos - 1) / allCombos) * (1 / allCombos) + ((allCombos - 1) / allCombos)
-      * ((allCombos - 1) / allCombos) * (1 / allCombos))
-print("b)",
-      (1 / noZeroBegin) + ((noZeroBegin - 1) / noZeroBegin) * (1 / noZeroBegin) + ((noZeroBegin - 1) / noZeroBegin)
-      * ((noZeroBegin - 1) / noZeroBegin) * (1 / noZeroBegin))
+print("a)", a)
+print("b)", b)
