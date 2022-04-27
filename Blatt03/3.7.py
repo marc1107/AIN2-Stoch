49cde6e7dedbe131571ca3a89ff2629456f79826
marc1107 <m.bohner99@gmail.com>
2022-04-27 12:57:15 +0200

    Aufgabe 3.7.1

---
+++ Blatt03/3.7.py
@@ -0,0 +1,25 @@
+def fak(n: int):
+    if n > 0:
+        return n * fak(n - 1)
+    else:
+        return 1
+
+
+def fak2(n: int):
+    if n % 2 != 0:
+        raise Exception("nicht durch 2 teilbar!")
+    else:
+        if n > 0:
+            return n * fak2(n - 2)
+        else:
+            return 1
+
+
+print("3.7.1")
+# a)
+print("a)", fak(20))
+
+# b)
+print("\nb)", fak2(20))
+
+print("\nc)", 10 * fak(10) * fak(10))
