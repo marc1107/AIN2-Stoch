0fd42c6d08286b98ec68cd7b13e1446b1763e24d
marc1107 <m.bohner99@gmail.com>
2022-06-02 10:40:20 +0200

    Pruefungsvorbereitung

--- Pruefung/test.py
+++ Pruefung/test.py
@@ -1,13 +1,13 @@
 import marcpy as mp
 
 counter = 0
-
 for i in range(0, 1000):
     counter += mp.binomialverteilt(1000, 0.005, i)
     if counter >= 0.99:
         print("\n", i)
         break
 
+counter = 0
 for i in range(0, 1000):
     counter += mp.binomialverteilt(1000, 0.00075, i)
     if counter >= 0.99:
